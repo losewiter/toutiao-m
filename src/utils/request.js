aaf09dca1a66dbf608aa5a9276ac12db745ba45e
losewiter <losewiter@163.com>
2022-01-22 14:02:21 +0800

    token刷新

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,9 +3,11 @@
  */
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
 
 const request = axios.create({
   baseURL: 'http://toutiao.itheima.net'
+  // baseURL: 'http://api-toutiao-web.itheima.net/app'
 })
 
 // 请求拦截器
@@ -23,6 +25,41 @@ request.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
 // 响应拦截器
+request.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  async function (error) {
+    if (error.response && error.response.status === 401) {
+      const user = store.state.user
+      console.log(user)
+      if (!user || !user.refresh_token) {
+        router.push('/login')
+      }
+      console.log(user.refresh_token)
+      try {
+        const res = await axios({
+          url: 'http://toutiao.itheima.net/v1_0/authorizations',
+          method: 'put',
+          headers: {
+            Authorization: `Bearer ${user.refresh_token}`
+          }
+        })
+        console.log(res)
+        store.commit('setUser', {
+          token: res.data.data.token,
+          refresh_token: user.refresh_token
+        })
+        return request(error.config)
+      } catch (err) {
+        console.log(err)
+        // router.push('/login')``
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default request
